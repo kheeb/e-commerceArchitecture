7417e138ada6bbcaa11f77a32e9e5c60c7118545
kheeb <93744725+kheeb@users.noreply.github.com>
2022-02-22 16:40:34 -0500

    added tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,54 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findByPk(req.params.id, {
+      include: [
+        { model: Category },
+        { model: Tag },
+      ]
+    });
+    if (!tagInfo) {
+      res.status(404).json({ message: 'No data found.'});
+      return;
+    }
+    res.status(200).json(tagInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagInfo = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!tagInfo) {
+      res.status(404).json({ message: 'No data found.'});
+      return;
+    }
+    res.status(200).json(tagInfo);
+  } catch (err) {
+    res.status.apply(500).json(err);
+  }
 });
 
 module.exports = router;
